093a3820c1f0a9ac9c0fc23cd8962c8a9f82618f
wfy <1712619114@qq.com>
2025-02-09 17:53:21 +0800

    lab4-1-exam

--- lab4-1-exam/user/lib/ipc.c
+++ lab4-1-exam/user/lib/ipc.c
@@ -1,9 +1,7 @@
-// User-level IPC library routines
-
+// User-level IPC library routine
 #include <env.h>
 #include <lib.h>
 #include <mmu.h>
-
 // Send val to whom.  This function keeps trying until
 // it succeeds.  It should panic() on any error other than
 // -E_IPC_NOT_RECV.
@@ -36,4 +34,25 @@ u_int ipc_recv(u_int *whom, void *dstva, u_int *perm) {
 	}
 
 	return env->env_ipc_value;
+}
+void sem_open(int sem_id, int n) {
+	syscall_sem_open(sem_id, n);
+}
+
+int sem_wait(int sem_id) {
+	int r;
+	// Lab 4-1-Exam: Your code here. (1/9)
+	// Implement process blocking
+	while ((r = syscall_sem_wait(sem_id)) == -1) {
+                 syscall_yield();
+	}
+	return r;
+}
+
+int sem_post(int sem_id) {
+	return syscall_sem_post(sem_id);
+}
+
+int sem_kill(int sem_id) {
+	return syscall_sem_kill(sem_id);
 }
